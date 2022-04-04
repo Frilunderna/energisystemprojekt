dcf78f3afff2e6f8bd0d481573b81e42a5a96e6f
Frilunderna <frilund@student.chalmers.se>
2022-04-04 15:06:57 +0200

    early commit

--- src/energisystemprojekt.jl
+++ src/energisystemprojekt.jl
@@ -1,10 +1,10 @@
-# I den här filen bygger ni modellen. Notera att det är skrivet som en modul, dvs ett paket. 
+# I den här filen bygger ni modellen. Notera att det är skrivet som en modul, dvs ett paket.
 # Så när ni ska använda det, så skriver ni Using energisystemprojekt i er REPL, då får ni ut det ni
 # exporterat. Se rad 9.
 
 module energisystemprojekt
 
-using JuMP, AxisArrays, Gurobi, UnPack
+using JuMP, AxisArrays, Gurobi, UnPack, Plots
 
 export runmodel
 
@@ -13,8 +13,8 @@ include("input_energisystemprojekt.jl")
 function buildmodel(input)
 
     println("\nBuilding model...")
- 
-    @unpack REGION, PLANT, HOUR, numregions, load, maxcap = input
+
+    @unpack REGION, PLANT, HOUR, numregions, load, maxcap, annualcost, runcost, fucost, hydro_inflow, pv_cf, wind_cf, ef_gas = input
 
     m = Model(Gurobi.Optimizer)
 
@@ -22,6 +22,9 @@ function buildmodel(input)
 
         Electricity[r in REGION, p in PLANT, h in HOUR]       >= 0        # MWh/h
         Capacity[r in REGION, p in PLANT]                     >= 0        # MW
+        Reservoar[h in HOUR]                                  >= 0        # MWh
+        Systemcost[r in REGION]                               >= 0        # Euro
+        Emissions[r in REGION]                                >= 0        # Ton CO2
 
     end #variables
 
@@ -30,15 +33,33 @@ function buildmodel(input)
     for r in REGION, p in PLANT
         set_upper_bound(Capacity[r, p], maxcap[r, p])
     end
+    for h in HOUR
+        set_upper_bound(Reservoar[h],33*1000000)
+    end
 
 
     @constraints m begin
-        Generation[r in REGION, p in PLANT, h in HOUR],
-            Electricity[r, p, h] <= Capacity[r, p] # * capacity factor
-
+        Generation_hydro[r in REGION, h in HOUR],
+            Electricity[r, :Hydro, h] <= Capacity[r, :Hydro]
+        Generation_gas[r in REGION, h in HOUR],
+            Electricity[r, :Gas, h] <= Capacity[r, :Gas]
+        Generation_pv[r in REGION, h in HOUR],
+            Electricity[r, :PV, h] <= Capacity[r, :PV] * pv_cf[r, h]
+        Generation_wind[r in REGION, h in HOUR],
+            Electricity[r, :Wind, h] <= Capacity[r, :Wind] * wind_cf[r, h]
+        Demand[r in REGION, h in HOUR],
+            sum(Electricity[r, p, h] for p in PLANT) >= load[r,h]
         SystemCost[r in REGION],
-            Systemcost[r] >= 0 # sum of all annualized costs
-    
+            Systemcost[r] >= sum(Capacity[r, p]*annualcost[p] for p in PLANT) + sum(Electricity[r, p, h] * (runcost[p] + fucost[p]) for p in PLANT, h in HOUR) # sum of all annualized costs
+        Reserv1[h in 2:length(HOUR)],
+            Reservoar[h] == Reservoar[h-1]+hydro_inflow[h]-Electricity[:SE,:Hydro,h]
+        Reserv2[h in HOUR],
+            sum(Electricity[r,:Hydro,h] for r in REGION) <= Reservoar[h]
+        Reserv3,
+            Reservoar[1] == Reservoar[length(HOUR)]
+        Gas_emissions[r in REGION],
+            Emissions[r] == sum(Electricity[r, :Gas, h] for h in HOUR) * ef_gas
+
     end #constraints
 
 
@@ -46,25 +67,25 @@ function buildmodel(input)
         sum(Systemcost[r] for r in REGION)
     end # objective
 
-    return (;m, Capacity)
+    return (;m, Capacity, Emissions, Electricity)
 
 end # buildmodel
 
-function runmodel() 
+function runmodel()
 
     input = read_input()
 
     model = buildmodel(input)
 
-    @unpack m, Capacity = model   
-    
+    @unpack m, Capacity, Emissions, Electricity = model
+
     println("\nSolving model...")
-    
+
     status = optimize!(m)
-    
+
 
     if termination_status(m) == MOI.OPTIMAL
-        println("\nSolve status: Optimal")   
+        println("\nSolve status: Optimal")
     elseif termination_status(m) == MOI.TIME_LIMIT && has_values(m)
         println("\nSolve status: Reached the time-limit")
     else
@@ -73,15 +94,35 @@ function runmodel()
 
     Cost_result = objective_value(m)/1000000 # M€
     Capacity_result = value.(Capacity)
-
+    emissions_result = value.(Emissions)
+    el=value.(Electricity)
 
     println("Cost (M€): ", Cost_result)
-   
+    println(Capacity_result)
+    println(emissions_result, " ton CO2")
+
+#    for r in REGION
+#        y = sum(Capacity[p ,r] for p in PLANT)
+#        x = HOUR[1,length(HOUR)]
+#        plot(x,y)
+#    end
+# start of plot for exercise 1
+
+    x = 147:160#651
+    y_w = value.(Electricity[:DE, :Wind, x])
+    y_pv = zeros(length(x))
+    y_gas = zeros(length(x))
+    for z in 1:length(x)
+        y_pv[z] = value.(Electricity[:DE, :Wind, z]) + value.(Electricity[:DE, :PV, z])
+        y_gas[z] = y_pv[z] + value.(Electricity[:DE, :Gas, z])
+    end
+
+    plot(x,y_gas)
+    plot(x,y_pv)
+    plot(x,y_w)
     nothing
 
 end #runmodel
 
 
-
 end # module
-
--- src/input_energisystemprojekt.jl
+++ src/input_energisystemprojekt.jl
@@ -1,4 +1,4 @@
-# I den här filen kan ni stoppa all inputdata. 
+# I den här filen kan ni stoppa all inputdata.
 # Läs in datan ni fått som ligger på Canvas genom att använda paketen CSV och DataFrames
 
 using CSV, DataFrames
@@ -9,7 +9,7 @@ folder = dirname(@__FILE__)
 
 #Sets
 REGION = [:DE, :SE, :DK]
-PLANT = [:Hydro, :Gas] # Add all plants
+PLANT = [:Hydro, :Gas, :PV, :Wind] # Add all plants
 HOUR = 1:8760
 
 #Parameters
@@ -19,25 +19,80 @@ numhours = length(HOUR)
 timeseries = CSV.read("$folder\\TimeSeries.csv", DataFrame)
 wind_cf = AxisArray(ones(numregions, numhours), REGION, HOUR)
 load = AxisArray(zeros(numregions, numhours), REGION, HOUR)
- 
+pv_cf = AxisArray(ones(numregions, numhours), REGION, HOUR)
+hydro_inflow = AxisArray(timeseries[:,"Hydro_inflow"], HOUR)
+
+
     for r in REGION
-        wind_cf[r, :]=timeseries[:, "Wind_"*"$r"]                                                        # 0-1, share of installed cap
-        load[r, :]=timeseries[:, "Load_"*"$r"]                                                           # [MWh]
+        wind_cf[r, :]=timeseries[:, "Wind_"*"$r"]                             # 0-1, share of installed cap
+        pv_cf[r, :]=timeseries[:, "PV_"*"$r"]
+        load[r, :]=timeseries[:, "Load_"*"$r"]                                # [MWh]
     end
 
-myinf = 1e8
+
 maxcaptable = [                                                             # GW
-        # PLANT      DE             SE              DK       
-        :Hydro       0              14              0       
-        :Gas         myinf          myinf           myinf         
+        # PLANT      DE             SE              DK
+        :Hydro       0              14              0
+        :Gas         10000          10000           10000
+        :PV          460            75              60
+        :Wind        180            280             90
         ]
 
 maxcap = AxisArray(maxcaptable[:,2:end]'.*1000, REGION, PLANT) # MW
 
+investmentcost = [              #euro/KW
+        # Plant
+        :Hydro          0
+        :Gas            550
+        :PV             600
+        :Wind           1100
+        ]
+invcost = AxisArray(vec(investmentcost[:,2]'./1000), PLANT) #euro/MW
 
-discountrate=0.05
+runningcost = [                 #euro/MWh
+        # Plant
+        :Hydro          0.1
+        :Gas            2
+        :PV             0.1
+        :Wind           0.1
+        ]
+runcost = AxisArray(vec(runningcost[:,2]), PLANT) #euro/MWh
+
+fuelcost = [                     #euro/MWh_el
+        :Hydro          0
+        :Gas            22/0.4
+        :PV             0
+        :Wind           0
+        ]
+fucost = AxisArray(vec(fuelcost[:,2]), PLANT) #euro/MWh_el
+
+lifetime = [
+        :Hydro          80
+        :Gas            30
+        :PV             25
+        :Wind           25
+        ]
+
+lt = AxisArray(vec(lifetime[:,2]), PLANT) #years
+
+efficiency =  [
+        :Hydro          1
+        :Gas            0.4
+        :PV             1
+        :Wind           1
+        ]
+eff = AxisArray(vec(efficiency[:,2]), PLANT) #MWh/MWh
+
+ef_gas = 0.202
+
+discountrate = 0.05
+
+annualcost= AxisArray(ones(length(PLANT)), PLANT)
+for p in PLANT
+        annualcost[p]=invcost[p]*discountrate/(1-(1/((1+discountrate)^lt[p])))
+end
 
 
-      return (; REGION, PLANT, HOUR, numregions, load, maxcap)
+      return (; REGION, PLANT, HOUR, numregions, load, maxcap, annualcost, runcost, fucost, hydro_inflow, pv_cf, wind_cf, ef_gas)
 
 end # read_input
